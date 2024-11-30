8d8bf9b2e5eaff0b5f08c4de4373689f4d76120e
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-11-30 15:20:59 +0300

    disable mypy

--- src/rag/vector_store.py
+++ src/rag/vector_store.py
@@ -4,5 +4,7 @@ from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
 
 vector_store = Chroma(
-    embedding_function=OllamaEmbeddings(model="nomic-embed-text", base_url=os.environ.get("EMBEDDINGS_BASE_URL"))
+    embedding_function=OllamaEmbeddings(
+        model="mxbai-embed-large:latest", base_url=os.environ.get("EMBEDDINGS_API_BASE")
+    )
 )
